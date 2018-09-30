98502ad9bec64d6658652267de8bc5f25ee1e011
Veronica <vsalm@ualberta.ca>
2018-09-30 15:50:46 -0600

    Got a few things working

--- app/src/main/java/com/example/veronica/vsalm_feelsbook/MainActivity.java
+++ app/src/main/java/com/example/veronica/vsalm_feelsbook/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.veronica.vsalm_feelsbook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -13,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        toolbar.setNavigationIcon(R.drawable.ic_launcher_foreground);
+        toolbar.setTitle(R.string.app_name);
     }
 
     public void viewEmotion(View view) {
@@ -22,5 +30,12 @@ public class MainActivity extends AppCompatActivity {
 //        String message = editText.getText().toString();
 //        intent.putExtra("Hello World", helloworld);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/veronica/vsalm_feelsbook/ViewEmotionActivity.java
+++ app/src/main/java/com/example/veronica/vsalm_feelsbook/ViewEmotionActivity.java
@@ -3,6 +3,7 @@ package com.example.veronica.vsalm_feelsbook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class ViewEmotionActivity extends AppCompatActivity {
@@ -11,6 +12,11 @@ public class ViewEmotionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_emotion);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        toolbar.setNavigationIcon(R.drawable.ic_launcher_foreground);
+        toolbar.setTitle(R.string.app_name);
 
         TextView viewEmotionText = findViewById(R.id.viewEmotionText);
         viewEmotionText.setText("Hello you made it!");
